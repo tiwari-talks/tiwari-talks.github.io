71d7a12d109a096e0ee1ffdeadbcc6ed94f14b24
Kushagra Raj <kushagraraj6323@gmail.com>
2021-01-19 10:35:48 +0530

    ✨ Initial Commit

---
+++ docusaurus.config.js
@@ -0,0 +1,76 @@
+module.exports = {
+  title: 'Tiwari Talks',
+  tagline: '',
+  url: '',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'venomish69', // Usually your GitHub org/user name.
+  projectName: 'tiwari-talks', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Tiwari Talks',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {to: 'blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Community',
+          items: [
+            
+            {
+              label: 'Discord',
+              href: 'https://discord.gg/dWCBpmhjuE',
+            },
+            {
+              label: 'Twitter',
+              href: '',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'Instagram',
+              href: 'https://www.instagram.com/venomish05/',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: false,
+        blog: {
+          path: './blog',
+          routeBasePath: '/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
